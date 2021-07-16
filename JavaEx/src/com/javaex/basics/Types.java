32058dd9d89e59d5807cb8a66f3d47d29ab936dc
김나래 <narae900@naver.com>
2021-07-16 14:49:53 +0900

    상수 연습

--- JavaEx/src/com/javaex/basics/Types.java
+++ JavaEx/src/com/javaex/basics/Types.java
@@ -3,14 +3,30 @@ package com.javaex.basics;
 // 변수, 예제
 // 데이터 타입 예제
 public class Types {
+	static final int SPEED_LIMIT = 120; 
 	public static void main(String[] args) {
 //		varEx();
 //		integerEx();
 //		floatDoubleEx();
 //		booleanEx();
-		charEx();	
+//		charEx();	
+		constantEx();
 		
 	}
+	// 상수
+	private static void constantEx() {
+//		final int SPEED_LIMIT = 120; // 코드의 가독성과 코드의 변경이 용이해진다 - 맨 첫줄에 적용하기
+		
+		System.out.println("제한속도:"+ SPEED_LIMIT);		
+		System.out.println("현재 도로의 제한 속도는" + SPEED_LIMIT + "입니다."); 
+		
+//		SPEED_LIMIT = 160; // 변경 시도 -> final 적용하면 변경 시도 불가
+		
+//		System.out.println("제한속도:"+ SPEED_LIMIT);		
+//		System.out.println("현재 도로의 제한 속도는" + SPEED_LIMIT + "입니다.");  // 이런 경우를 막기 위해 final 사용 
+		
+	}
+	
 	
 	//문자형 데이터 타입
 	private static void charEx() {
