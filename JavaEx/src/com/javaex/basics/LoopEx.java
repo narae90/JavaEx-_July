9e87d0c9b7337290c2333cf0b48432a5fbee2c1f
BIT <BIT@DESKTOP-NRL27PI>
2021-07-20 17:17:22 +0900

    반복문

--- JavaEx/src/com/javaex/basics/LoopEx.java
+++ JavaEx/src/com/javaex/basics/LoopEx.java
@@ -7,10 +7,57 @@ public class LoopEx {
 	public static void main(String[] args) {
 //		whileEx();
 //		whileGugu();
-		dowhileEx();
+//		dowhileEx();
+//		forGugu();
+//		continueEx();
+//		Practice01();
+		Practice02();
+		
 		System.out.println("End of Code");
 
 	}
+	private static void Practice01() {
+		
+		for(int dan=2; dan <=9; dan++) 
+		for(int i = 1; i<=9; i++) {	
+			System.out.println(dan + "*" + i + "=" + (dan*i));
+		}
+	}
+	
+	private static void Practice02() {
+		
+		for(int i=1; i<=5; i++) {
+				System.out.println("*");
+				
+				
+			}
+		}
+	
+	
+	private static void continueEx() {
+		for(int i= 1; i <= 20; i++) {
+			if(i % 2 == 0 || i % 3 == 0)
+				continue;
+			System.out.println(i);
+		}
+		
+	}
+	
+	private static void forGugu() {
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("단을 입력하세요.");
+		int dan = scanner.nextInt();
+		
+		for(int i = 1; // 초기화 
+			i <=9; //반복 조건 
+			i++) { //증감식	
+			System.out.println(dan + "*" + i + "=" + (dan*i));
+		} 
+
+		scanner.close();
+	}
+	
 	
 	private static void dowhileEx() {
 		// 적어도 한번은 수행되어야 하는 반복,
