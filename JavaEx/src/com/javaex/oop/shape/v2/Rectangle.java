687323c4d042d4c5a37383d1340966c74418318d
김나래 <narae900@naver.com>
2021-07-23 16:29:23 +0900

    추상 클래스와 인터페이스

---
+++ JavaEx/src/com/javaex/oop/shape/v2/Rectangle.java
@@ -0,0 +1,32 @@
+package com.javaex.oop.shape.v2;
+
+// 추상 클래스를 상속받은 클래스는 반드시 추상 메소드를 구현해야 한다.
+public class Rectangle extends Shape implements Drawable {
+	// 필드
+	protected int width;
+	protected int height;
+	
+	
+	// 생성자
+	public Rectangle(int x, int y, int width, int height) {
+		super(x, y);
+		this.width =width;
+		this.height=height;
+		
+	}
+
+		// 추상 메소드 오버라이드 구현(필수)
+	@Override
+	public double area() {
+		return width * height;
+	}
+	
+
+	@Override
+	public void draw() {
+		System.out.printf(
+			"사각형[x=%d, y=%d, w=%d, h=%d, area=%f]을 그렸어요%n",
+			x, y, width, height, area());
+	}
+	}
+
