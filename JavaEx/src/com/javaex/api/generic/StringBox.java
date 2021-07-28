95e2275df1dd3723501eeafad3e7e291d823be46
BIT <BIT@DESKTOP-NRL27PI>
2021-07-28 14:48:13 +0900

    Date, Calendar, Generic

---
+++ JavaEx/src/com/javaex/api/generic/StringBox.java
@@ -0,0 +1,17 @@
+package com.javaex.api.generic;
+
+public class StringBox {
+	
+	String content;
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+
+}
